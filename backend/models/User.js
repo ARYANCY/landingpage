dd8c84809f66e37a77fe321f70b4c47128142af9
ARYANCY <aryan.crores@gmail.com>
2025-09-04 16:29:06 +0530

    improved styling

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema({
   googleId: { type: String, required: true },
   name: String,
   email: String,
-  avatar: String
+  avatar: String,
+  number: Number
 });
 
 module.exports = mongoose.model('User', userSchema);
