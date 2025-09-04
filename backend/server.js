e2e51dff5d73de3f881650333ab4c1775b8cd829
ARYANCY <aryan.crores@gmail.com>
2025-09-04 12:33:07 +0530

    chh

--- backend/server.js
+++ backend/server.js
@@ -4,36 +4,47 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const session = require('express-session');
 const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 dotenv.config();
 require('./config/passport');
 
 const authRoutes = require('./routes/auth');
 const apiRoutes = require('./routes/api');
+
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 if (process.env.NODE_ENV === 'production') {
   app.set('trust proxy', 1);
 }
+
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'dev_insecure_secret_change_me',
+  secret: process.env.SESSION_SECRET ,
   resave: false,
   saveUninitialized: false,
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    collectionName: 'sessions',
+    ttl: 24 * 60 * 60
+  }),
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
     secure: process.env.NODE_ENV === 'production'
   }
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/auth', authRoutes);
 app.use('/api', apiRoutes);
-
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
